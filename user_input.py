8ec30382d202aec135f90da2920ebe4b3aea7170
Orado Wiseman <145492701+kdotgoat@users.noreply.github.com>
2024-09-20 23:04:11 +0300

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -1,4 +1,4 @@
 name = input("Your name?: ")
 age = input("Your age?: ")
-location = input("Your location?: ")
+location = input("Your location ?: ")
 print("Hello " + name + "," +"you are " + age + " years old and live in " + location)
